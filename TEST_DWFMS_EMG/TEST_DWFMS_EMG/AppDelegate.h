b5743c4de128ba666f0aa69e827b48928cabd878
kimhyang <gim-hyang-gi@gimhyang-gis-Mac.local>
2015-04-25 21:05:39 +0900

    create

---
+++ TEST_DWFMS_EMG/TEST_DWFMS_EMG/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TEST_DWFMS_EMG
+//
+//  Created by 김향기 on 2015. 4. 25..
+//  Copyright (c) 2015년 kimhyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TEST_DWFMS_EMG/TEST_DWFMS_EMG/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TEST_DWFMS_EMG
+//
+//  Created by 김향기 on 2015. 4. 25..
+//  Copyright (c) 2015년 kimhyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
